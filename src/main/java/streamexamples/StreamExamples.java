26b6d8681887ebe1d55016ae7615d088689cc35a
Dennis Jakobi <dennis.jakobi76@gmail.com>
2025-12-17 15:46:55 +0100

    first commit

---
+++ src/main/java/streamexamples/StreamExamples.java
@@ -0,0 +1,10 @@
+package streamexamples;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExamples {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1,4,10,3,6,1,15,4,9,16,5);
+    }
+}
